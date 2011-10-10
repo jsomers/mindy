2e664f8a74f70b4ee2e6a8bc39be4aa22d99ebb2
James Somers <jsomers@gmail.com>
2011-10-10 19:45:35 -0400

    Initial Rails layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mindycoat::Application.config.secret_token = '513037d4e4420a56a7acbc00fe3ea9b4297b8d573e140dde67bad4caef358ba425f230748d511c4141440896e25a625ac9362b74d7636b99ee72b5998f96336b'
